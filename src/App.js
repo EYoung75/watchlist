448c85a09ce905547f47f3509bc8b5a876b2cf77
EYoung75 <eyoung75.ey@gmail.com>
2020-03-11 11:12:36 -0500

    rendering carousel for trending movies with intial mobile first styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./main.scss";
 import Header from "./components/header.jsx";
 import Navbar from "./components/navbar.jsx";
 import Trending from "./pages/trending.jsx";
+import TrendingMovies from "./components/trendMovies";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="landing__container">
-        <Navbar />
+        {/* <Navbar className="navbar"/> */}
         <Trending />
+        <TrendingMovies/>
       </div>
     </div>
   );
