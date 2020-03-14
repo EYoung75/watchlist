6e4009d6eb44a9c1384d3d8259781d98ce2d76d9
EYoung75 <eyoung75.ey@gmail.com>
2020-03-14 17:10:26 -0500

    Storing all desired information for any given movie, working on layout and desired format

--- src/pages/movieDetails.jsx
+++ src/pages/movieDetails.jsx
@@ -6,6 +6,10 @@ const MovieDetails = props => {
   const [movieID, setMovieID] = useState(props.match.params["id"]);
   const [movieDetails, setMovieDetails] = useState([]);
   const [movieTrailer, setMovieTrailer] = useState([]);
+  const [movieReviews, setMovieReviews] = useState([]);
+  const [relatedMovies, setRelatedMovies] = useState([]);
+  const [movieCast, setMovieCast] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     fetchMovieDetails();
@@ -18,18 +22,31 @@ const MovieDetails = props => {
     )
       .then(res => res.json())
       .then(res => setMovieDetails(res));
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-    .then(res => res.json())
-    .then(res => setMovieTrailer(res.results[0]))
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}/reviews?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then(res => res.json())
+      .then(res => setMovieTrailer(res.results[0]));
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then(res => res.json())
+      .then(res => setRelatedMovies(res.results));
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then(res => res.json())
+      .then(res => setMovieCast(res.cast));
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/reviews?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then(res => res.json())
+      .then(res => setMovieReviews(res.results));
   }
-
 
   return (
     <div className="movieDetails">
-      {console.log(movieDetails)}
+      {console.log(relatedMovies)}
       <div className="movieDetails__backdrop">
         <Link to="/" className="movieDetails__backdrop__backButton">
           <FaArrowAltCircleLeft />
@@ -46,6 +63,7 @@ const MovieDetails = props => {
       </div>
       <div className="movieDetails__overview">
         <p>{movieDetails["tagline"]}</p>
+        {JSON.stringify(movieDetails.genres)}
         <div className="movieDetails__overview__vote">
           <FaStar />
           <p>{movieDetails["vote_average"]}</p>
@@ -55,12 +73,42 @@ const MovieDetails = props => {
           <p>{movieDetails["release_date"]}</p>
           <p>{"Runtime: " + movieDetails["runtime"] + " minutes"}</p>
         </div>
-        <p>{JSON.stringify(movieDetails["genres"])}</p>
+        {/* <p>{JSON.stringify(movieDetails["genres"])}</p> */}
+
         <div className="movieDetails__overview__synopsis">
           <h3>Synopsis:</h3>
           <p>{movieDetails["overview"]}</p>
         </div>
-        <iframe className="movieDetails__overview__trailer" src={`https://www.${movieTrailer.site}.com/embed/${movieTrailer.key}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          className="movieDetails__overview__trailer"
+          src={`https://www.${movieTrailer.site}.com/embed/${movieTrailer.key}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <div className="movieDetails__cast">
+          <h3>Cast:</h3>
+          {movieCast.map(cast => (
+            <p>{cast.character}</p>
+          ))}
+        </div>
+        <div className="movieDetails__reviews">
+          <h3>Reviews:</h3>
+          {movieReviews.map(review => (
+            <div className="movieDetails__reviews__review">
+              <h4>{review.author}</h4>
+              <p>{review.content}</p>
+            </div>
+          ))}
+        </div>
+        <div className="movieDetails__related">
+          <h3>Related Movies:</h3>
+          {relatedMovies.map(title => (
+            <div className="movieDetails__related__card">
+              <h4>{title.title}</h4>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
