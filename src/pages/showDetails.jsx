c9bbd92b7f91385263101ebb2502ea547074d1d1
EYoung75 <eyoung75.ey@gmail.com>
2020-03-16 12:57:47 -0500

    rendering show data and formatting desired layout for showcasing season and episode data

--- src/pages/showDetails.jsx
+++ src/pages/showDetails.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const ShowDetails = props => {
   const [showID, setShowID] = useState(props.match.params["id"]);
@@ -13,15 +15,34 @@ const ShowDetails = props => {
       `https://api.themoviedb.org/3/tv/${showID}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
     )
       .then(res => res.json())
-      .then(res => setShowDetails(res))
+      .then(res => setShowDetails(res));
   }
-  return <div className="showDetails">
-       <img
-            src={"https://image.tmdb.org/t/p/original/" + showDetails["backdrop_path"]}
-          />
+  return (
+    <div className="showDetails">
+      <div className="showDetails__backdrop">
+        <Link to="/" className="showDetails__backdrop__backButton">
+          <FaArrowAltCircleLeft />
+        </Link>
+        <img
+          src={
+            "https://image.tmdb.org/t/p/original/" +
+            showDetails["backdrop_path"]
+          }
+        />
         <h1>{showDetails["name"]}</h1>
-        {console.log(JSON.stringify(showDetails))}
-      </div>;
+      </div>
+      <div className="showDetails__intial">
+        <h3>First Aired: {showDetails["first_air_date"]}</h3>
+        <p>
+          {showDetails["in_production"] == true ? "Still running" : "Finished"}
+        </p>
+        <p>{showDetails["number_of_seasons"] + " seasons"}</p>
+        <p>{showDetails["number_of_episodes"] + " episodes"}</p>
+        <h3>Overview:</h3>
+        <p>{showDetails["overview"]}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ShowDetails;
