32c45a7a4ed91e686f93fbea5f315213288ed250
EYoung75 <eyoung75.ey@gmail.com>
2020-03-29 10:24:54 -0500

    Provided correct dynamic url endpoint for fetching season details

--- src/pages/seasonDetails.jsx
+++ src/pages/seasonDetails.jsx
@@ -6,12 +6,12 @@ function SeasonDetails(props) {
   const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
-    fetchSeasonDetails(props.match.params["season"]);
+    fetchSeasonDetails(props.match.params);
   }, []);
 
   async function fetchSeasonDetails(season) {
     fetch(
-      `https://api.themoviedb.org/3/tv/63247/season/${season}?api_key=${process.env.REACT_APP_API_KEY}`
+      `https://api.themoviedb.org/3/tv/${season["id"]}/season/${season["season"]}?api_key=${process.env.REACT_APP_API_KEY}`
     )
       .then(res => res.json())
       .then(res => setEpisodes(res.episodes));
